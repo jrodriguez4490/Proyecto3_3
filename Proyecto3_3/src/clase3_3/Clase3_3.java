4cc5ccc7f7f6bbeb72c7b5aa5bb570a84d847b3f
jrodriguez4490 <jrodriguez4490@ceuandalucia.es>
2023-11-30 02:23:02 +0100

    Cambios en Clase3_3

--- Proyecto3_3/src/clase3_3/Clase3_3.java
+++ Proyecto3_3/src/clase3_3/Clase3_3.java
@@ -2,6 +2,6 @@ package clase3_3;
 
 public class Clase3_3 {
 public static void main(String[] args) {
-	System.out.println("Práctica 3_3. Jose Rodríguez Torrejón.");
+	System.out.println("Canela En Rama");
 }
 }
